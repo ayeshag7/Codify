3b602c15b82bdbd976abbe53b0d71f96922c8c85
Ayesha <ayeshaiqbal2592@gmail.com>
2023-09-16 13:44:27 +0500

    Modified some UI components

--- pages/index.js
+++ pages/index.js
@@ -26,13 +26,13 @@ export default function Home({ data }) {
 
       <div className="flex flex-col gap-y-16 ml:flex-row ml:justify-between px-4 ml:px-16 my-20">
 
-        <div className="flex justify-around flex-wrap gap-x-4 gap-y-8 lg:flex-col lg:gap-y-4">
+        <div className="flex max-md:px-4 md:justify-around flex-wrap gap-x-4 gap-y-8 lg:flex-col lg:gap-y-4">
           {blogs.map((blog, index) => {
             return <BlogCard key={index} blog={blog} color={colors[index]}/>
           })}
         </div>
 
-        <div className="max-md:px-2 flex flex-col gap-y-14">
+        <div className="max-md:px-4 flex flex-col gap-y-14">
           <CreatorCard/>
           <FeaturedBlogs blogs={blogs}/>
           <FollowCard/>
